09e0841c020333c1ce9117d5214c7783cba9cbf5
Gavin Price <gaprice@lbl.gov>
2016-08-25 09:49:27 -0700

    handle null errors

--- src/us/kbase/auth2/lib/exceptions/AuthException.java
+++ src/us/kbase/auth2/lib/exceptions/AuthException.java
@@ -9,15 +9,23 @@ public class AuthException extends Exception {
 	private final AuthError err;
 	
 	public AuthException(final AuthError err, final String message) {
-		super(err.getErrorCode() + " " + err.getError() + ": " + message);
+		super(getMsg(err, message));
 		this.err = err;
+	}
+
+	private static String getMsg(final AuthError err, final String message) {
+		if (err == null) {
+			throw new NullPointerException("err");
+		}
+		return err.getErrorCode() + " " + err.getError() + 
+				(message == null || message.isEmpty() ? "" : ": " + message);
 	}
 	
 	public AuthException(
 			final AuthError err,
 			final String message,
 			final Throwable cause) {
-		super(err.getErrorCode() + " " + err.getError() + ": " + message,
+		super(getMsg(err, message),
 				cause);
 		this.err = err;
 	}
